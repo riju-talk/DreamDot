b92c7e61ee3efd5a959e2c906b018c008f2c6bad
riju-talk <talk.riju@gmail.com>
2025-06-12 17:56:33 +0530

    made changes to the repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone"
+};
 
 export default withNextVideo(nextConfig);
